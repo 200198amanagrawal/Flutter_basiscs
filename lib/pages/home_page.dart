2413a08992ad259f1991a2c2fe0b137320726899
amanagrawal <amanagrawal2098@gmail.com>
2022-12-04 18:21:26 +0530

    Contains all of the drawer that gets fetched from json fetches all of the value from json and then shows it to the homepage A load time is added because list is not instantly loaded with data from internet so Loading bar is added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,14 +1,42 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
 import 'package:flutter_catalog/widgets/items_widget.dart';
 
 import '../widgets/themes.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    await Future.delayed(Duration(
+        seconds:
+            2)); // this is added for one progress bar delay and no initialization error in list
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+    CatalogModel.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList(); // json to list converter
+    setState(
+        () {}); // this builds the the json changed value which we get from internet or from assginement
+  }
+
   @override
   Widget build(BuildContext context) {
-    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
     return MaterialApp(
       themeMode: ThemeMode.light,
       theme: MyTheme.lightTheme(context),
@@ -22,13 +50,17 @@ class HomePage extends StatelessWidget {
         ),
         body: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: ListView.builder(
-              itemCount: dummyList.length,
-              itemBuilder: (context, index) {
-                return ItemWidget(
-                  item: dummyList[index],
-                );
-              }),
+          child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
+              ? ListView.builder(
+                  itemCount: CatalogModel.items.length,
+                  itemBuilder: (context, index) {
+                    return ItemWidget(
+                      item: CatalogModel.items[index],
+                    );
+                  })
+              : Center(
+                  child: CircularProgressIndicator(),
+                ),
         ), //Body will serve as the middle element
         drawer: MyDrawer(), // Footer of the Scaffold
       ),
