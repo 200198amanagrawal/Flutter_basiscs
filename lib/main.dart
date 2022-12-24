d28e408eddb05e7c13869a6b1dc499bb0da238db
amanagrawal <amanagrawal2098@gmail.com>
2022-12-25 01:32:33 +0530

    a new detailed page which routes via clicking and the loading bar has been moved to center with better padding insets as well
    added a cart button with its navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/pages/cart_page.dart';
 import 'package:flutter_catalog/pages/home_page.dart';
 import 'package:flutter_catalog/pages/login_page.dart';
 import 'package:flutter_catalog/utils/routes.dart';
@@ -24,7 +25,8 @@ class MyApp extends StatelessWidget {
             HomePage(), // these pages are in psages folder and are imported where / will point to the main page if initiala route is not mentioned
         MyRoutes.homeRoute: (context) =>
             HomePage(), // these pages are in psages folder and are imported where / will point to the main page if initiala route is not mentioned
-        MyRoutes.loginRoute: (context) => LoginPage()
+        MyRoutes.loginRoute: (context) => LoginPage(),
+        MyRoutes.cartRoute: (context) => CartPage(),
       },
     );
   }
