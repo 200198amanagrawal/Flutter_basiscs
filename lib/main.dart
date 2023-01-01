01fefe1f37c10a72b8b376545ccb86642feebb43
amanagrawal <amanagrawal2098@gmail.com>
2023-01-01 23:28:41 +0530

    added a mutation where it'll act as a factory for other models

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/core/store.dart';
 import 'package:flutter_catalog/pages/cart_page.dart';
-import 'package:flutter_catalog/pages/home_page.dart';
 import 'package:flutter_catalog/pages/login_page.dart';
 import 'package:flutter_catalog/utils/routes.dart';
-import 'package:flutter_catalog/widgets/themes.dart';
+import 'package:velocity_x/velocity_x.dart';
+import 'pages/home_page.dart';
+import 'widgets/themes.dart';
 
 void main() {
-  runApp(MyApp()); // runApp Method which runs the main app
+  runApp(VxState(
+      store: MyStore(),
+      child: MyApp())); // runApp Method which runs the main app
 }
 
 //statles means the page is static with no change to show
--- lib/models/cart.dart
+++ lib/models/cart.dart
@@ -1,13 +1,8 @@
+import 'package:flutter_catalog/core/store.dart';
 import 'package:flutter_catalog/models/catalog.dart';
-import 'dart:convert';
+import 'package:velocity_x/velocity_x.dart';
 
 class CartModel {
-  static final cartModel = CartModel._internal();
-
-  CartModel._internal();
-
-  factory CartModel() => cartModel;
-
   // catalog field
   CatalogModel _catalog = CatalogModel();
 
@@ -29,15 +24,19 @@ class CartModel {
   num get totalPrice =>
       items.fold(0, (total, current) => total + current.price);
 
-  // Add Item
-
-  void add(Item item) {
-    _itemIds.add(item.id);
-  }
-
   // Remove Item
 
   void remove(Item item) {
     _itemIds.remove(item.id);
+  }
+}
+
+class AddMutation extends VxMutation<MyStore> {
+  final Item item;
+
+  AddMutation(this.item);
+  @override
+  perform() {
+    store?.cart._itemIds.add(item.id);
   }
 }
